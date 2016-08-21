1ce0e6c214a7e2e056f55cef32e0122117fa8b04
pluma11 <fertonani.oscar@gmail.com>
2016-08-21 14:10:15 -0300

    Add entity Domicilio
    Agrego la entidad domicilio

--- src/ejemploGit1/Persona.java
+++ src/ejemploGit1/Persona.java
@@ -4,6 +4,7 @@ public class Persona {
 
 	private String nombre;
 	private String apellido;
+	private Domicilio domicilio;
 
 	public Persona() {
 		super();
@@ -27,6 +28,14 @@ public class Persona {
 
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
+	}
+
+	public Domicilio getDomicilio() {
+		return domicilio;
+	}
+
+	public void setDomicilio(Domicilio domicilio) {
+		this.domicilio = domicilio;
 	}
 
 }
